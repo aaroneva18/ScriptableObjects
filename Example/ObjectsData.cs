e18895f405f5cbfe4b064dc9d966847d549ae440
aaroneva <aaronalvaradocalderon@gmail.com>
2025-02-05 23:28:02 -0600

    simplifying ObjectsData script

--- Example/ObjectsData.cs
+++ Example/ObjectsData.cs
@@ -5,17 +5,11 @@ using UnityEngine;
 public class ObjectsData : ScriptableObject
 {
     [SerializeField] private string objectName;
-    [SerializeField] private string ObjectID;
     [SerializeField] private string objectDescription;
     [SerializeField] private InteractableType interactableType;
 
     public string GetObjectName { get { return objectName; } }
     public string GetObjectDescription { get { return objectDescription; } }
-    public string GetObjectID { get { return ObjectID; } }
     public InteractableType GetInteractableType { get { return interactableType; } }
-
-    public void SetObjectID() {
-        ObjectID = objectName + Time.time + Random.Range(0, 1000);
-    }
 
 }
