fe1788c0754d97b919f91d347986162816d06bb1
aaroneva <aaronalvaradocalderon@gmail.com>
2025-02-05 23:01:33 -0600

    adding the code to Object Manager script

--- Example/ObjectManager.cs
+++ Example/ObjectManager.cs
@@ -1,1 +1,17 @@
 
+    using System;
+    using UnityEngine;
+
+    public class ObjectManager : MonoBehaviour
+    {
+        [SerializeField] private InteractableType interactableType;
+        [SerializeField] private ObjectsData objectData;
+
+        private void Awake() {
+            objectData.SetObjectID();
+            Debug.Log(objectData.GetObjectID);
+        }
+
+        public InteractableType GetInteractableType { get { return interactableType; } }
+
+    }
