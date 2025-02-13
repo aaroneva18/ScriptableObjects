35728a5d97ae995b06e784fa079d1a498a7e2808
aaroneva <aaronalvaradocalderon@gmail.com>
2025-02-12 22:26:48 -0600

    aadding UISrpite and UIPosition

--- Example/ObjectsData.cs
+++ Example/ObjectsData.cs
@@ -7,6 +7,8 @@ public class ObjectsData : ScriptableObject
     [SerializeField] private string objectName;
     [SerializeField] private string objectDescription;
     [SerializeField] private InteractableType interactableType;
+    [SerializeField] private Sprite UISprite;
+    [SerializeField] private Vector3 UIPosition;
 
     public string GetObjectName { get { return objectName; } }
     public string GetObjectDescription { get { return objectDescription; } }
